beab61c19c8c897f06d556118908ef80618e0ae4
apsschotten <ana.schotten@estudante.ifms.edu.br>
2025-03-27 14:13:47 -0400

    .

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDr1qsAALSjhJsnFMCRw2OVfEc6xxNIsPg",
+  authDomain: "trabalho-01-react-login.firebaseapp.com",
+  projectId: "trabalho-01-react-login",
+  storageBucket: "trabalho-01-react-login.firebasestorage.app",
+  messagingSenderId: "629776884340",
+  appId: "1:629776884340:web:527f26609da6053bbe2721",
+  measurementId: "G-M16JLLDWXN"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
